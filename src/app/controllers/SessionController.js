d63fac636b28adbd169ea7405f2d58d7c68a59f1
Henrico <hicolazu22@hotmail.com>
2020-03-10 16:56:43 -0300

    fix: bugfix in eslint rules

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -4,37 +4,37 @@ import User from '../models/User';
 import authConfig from '../../config/auth';
 
 class SessionController {
-    async store (req, res) {
-        const { email, password } = req.body;
-        
-        const user = await User.findOne({
-            where: { 
-                email
-             }
-        });
+  async store(req, res) {
+    const { email, password } = req.body;
 
-        if(!user) {
-            return res.status(401).json({ error: 'User not found!' });
-        }
+    const user = await User.findOne({
+      where: {
+        email,
+      },
+    });
 
-        if (await !user.checkPassword(password)){
-            return res.status(401).json({ error: 'Password does not match!' })
-        }
+    if (!user) {
+      return res.status(401).json({ error: 'User not found!' });
+    }
 
-        const { id, name } = user;
+    if (await !user.checkPassword(password)) {
+      return res.status(401).json({ error: 'Password does not match!' });
+    }
 
-        return res.json({
-            user: {
-                id,
-                name,
-                email,
-                password
-            },
-            token: jwt.sign({ id }, authConfig.secret, {
-                expiresIn: authConfig.expiresIn,       
-            }),
-        });
-    };
+    const { id, name } = user;
+
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+        password,
+      },
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    });
+  }
 }
 
 export default new SessionController();
